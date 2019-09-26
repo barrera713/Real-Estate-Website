585cc87db018a9e7412b6c43d6cf2d5041a5c956
Carlos Barrera <barrera9696@gmail.com>
2019-09-25 21:09:01 -0500

    frontend skeleton

---
+++ frontend/src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+
+    return (
+        <div>
+            <ul>
+                <li>Buy</li>
+                <li>Rent</li>
+            </ul>
+        </div>
+    )
+}
+
+export { NavBar }
